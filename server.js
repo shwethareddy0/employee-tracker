99e7edc2ee1218bbc315a92ac8f945a277c7b732
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-30 16:52:45 -0700

    create sql connection from server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employee_db",
+});
